e1c526d6d8b86ad0dd0307be0ff0b187f392bf22
Arun Murugan <arunmurugan200@gmail.com>
2021-10-27 23:22:01 +0530

    feat: fetch form using form id

--- src/routes/form.js
+++ src/routes/form.js
@@ -1,6 +1,7 @@
 const Form = require("../models/form");
 const { Router } = require("express");
 const validationErrorToErrors = require("../utils/validationErrorToErrors");
+const { ObjectID } = require("bson");
 
 const router = Router();
 
@@ -25,6 +26,24 @@ router.post("/new", async (req, res) => {
     console.error(e);
     res.status(500).send({ message: "Internal Server Error" });
   }
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (!ObjectID.isValid(id)) {
+    res.sendStatus(404);
+    return;
+  }
+
+  const form = await Form.findOne({ _id: id });
+
+  if (!form) {
+    res.sendStatus(404);
+    return;
+  }
+
+  res.send(form);
 });
 
 module.exports = router;
