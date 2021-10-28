fca1e0265cf82ee3d4c4a482745879726eefa1b4
Arun Murugan <arunmurugan200@gmail.com>
2021-10-28 10:21:45 +0530

    feat: load config from .env

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+// load env variables from .env file
+require("dotenv").config();
+
 const app = require("./server.js");
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log("listening at ", PORT);
